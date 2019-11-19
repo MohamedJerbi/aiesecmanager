539457e1b003c40373e7f4a1ccd58199efa30064
MohamedJerbi <mohamed.jerbi2012@gmail.com>
2019-11-19 23:37:07 +0100

    Updates

---
+++ precache-manifest.31bfa44b2d4c227fc5ad1c98de65c7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcae6ba5c48302629e210a6e29680b56",
+    "url": "/aiesecmanager/index.html"
+  },
+  {
+    "revision": "1752f99c524f794c10d5",
+    "url": "/aiesecmanager/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "146f8a40ba12c75513d5",
+    "url": "/aiesecmanager/static/js/2.ffe0c23a.chunk.js"
+  },
+  {
+    "revision": "1752f99c524f794c10d5",
+    "url": "/aiesecmanager/static/js/main.581217af.chunk.js"
+  },
+  {
+    "revision": "1363fb2e2084c0abe018",
+    "url": "/aiesecmanager/static/js/runtime-main.e4bf8248.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/aiesecmanager/static/media/logo.25bf045c.svg"
+  }
+]);
